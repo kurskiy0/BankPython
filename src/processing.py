f97cd017430601c8f2f322ea39fccce0e7627309
Igor <sungurov.igorext@gmail.com>
2025-01-26 00:36:58 +0300

    hw10.2

--- src/processing.py
+++ src/processing.py
@@ -2,13 +2,24 @@ def filter_by_state(list_dict: list[dict[str, any]], state_: str = "EXECUTED") -
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
     state соответствует указанному значению."""
     new_dict = []
+    if not list_dict:
+        return 'Не введены данные'
     for i in list_dict:
-        if i["state"] == state_:
+        if 'state' not in i:
+            return 'State отсутствует в списке словарей'
+        elif i["state"] == state_:
             new_dict.append(i)
     return new_dict
 
 
 def sort_by_date(list_: list, is_reverse: bool = True) -> list:
     """Функция возвращает новый список, отсортированный по дате"""
-    list_sorted = sorted(list_, key=lambda x: x["date"], reverse=is_reverse)
-    return list_sorted
+    if not list_:
+        return 'Не введены данные'
+    else:
+        for d in list_:
+            if 'date' in d:
+                list_sorted = sorted(list_, key=lambda x: x["date"], reverse=is_reverse)
+                return list_sorted
+            else:
+                return 'Отсутствует дата'
--- src/widget.py
+++ src/widget.py
@@ -5,24 +5,23 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(type_number: Union[str]) -> str:
     """Функция принимает строку, содержащую тип и номер карты или счета и возвращает строку с замаскированным номером"""
-    parts = type_number.split(' ')
-    count_str = parts[-1]
-    type_str = ' '.join(parts[0:-1])
-    if "Счет" in type_number or "Счёт" in type_number:
-        answer =  type_str + ' ' + get_mask_account(count_str)
+    if len(type_number) > 0:
+        parts = type_number.split(' ')
+        count_str = parts[-1]
+        type_str = ' '.join(parts[0:-1])
+        if "Счет" in type_number or "Счёт" in type_number:
+            answer =  type_str + ' ' + get_mask_account(count_str)
+        else:
+            answer = type_str + ' ' + get_mask_card_number(count_str)
+        return answer
     else:
-        answer = type_str + ' ' + get_mask_card_number(count_str)
-    return answer
-
+        return 'Введен некорректный номер счета или карты'
+    return None
 
 def get_date(date_old: Union[str]) -> str:
     """Функция принимает на вход полную дату и возвращает её в более удобном формате"""
-    date_splited = date_old.split("T")
-    date_ = date_splited[0].split('-')
-    return (f"{date_[-1]}.{date_[-2]}.{date_[-3]}")
-
-print(mask_account_card('Счет 12345678901234567890'))
-print(mask_account_card('Visa 1234567890123456'))
-print(mask_account_card('Visa Super 1234567890123456'))
-print(mask_account_card('Visa Super Puper 1234567890123456'))
-print(get_date('2012-11-10T01:02:03.012345'))
+    if len(date_old) == 26:
+        date_splited = date_old.split("T")
+        date_ = date_splited[0].split('-')
+        return (f"{date_[-1]}.{date_[-2]}.{date_[-3]}")
+    return 'Некорректный формат даты'
