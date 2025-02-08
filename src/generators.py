cac8a3f67972593b1790e9cde1623fdadb6132cd
Igor <sungurov.igorext@gmail.com>
2025-02-08 11:53:23 +0300

    hw 11.1 fixes

--- src/generators.py
+++ src/generators.py
@@ -1,10 +1,12 @@
-from typing import Union, Iterable, Iterator
+from typing import Union, Iterator
 from src.main import transactions
 
 
 def filter_by_currency(transactions: Union[list, dict], currency: str) -> Iterator:
     return (
-        transaction for transaction in transactions if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency
+        transaction
+        for transaction in transactions
+        if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency
     )
 
 
@@ -14,12 +16,12 @@ def transaction_descriptions(transactions: list) -> Iterator:
 
 
 def card_number_generator(start: int, end: int) -> Iterator:
-    for n in range(start, end+1):
+    for n in range(start, end + 1):
         card_number = str(n).zfill(16)
         yield f"{card_number[0:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:16]}"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     my_descriptions = transaction_descriptions(transactions)
     print(next(my_descriptions))
     print(next(my_descriptions))
